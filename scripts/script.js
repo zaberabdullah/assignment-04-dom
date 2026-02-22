b66a3a89f138d8c3018aa5e0193a5268025978e8
zaberabdullah <zaberabdullah120@gmail.com>
2026-02-22 14:57:21 +0600

    added all cards count

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,23 @@
-console.log('connected')
+// console.log('connected')
+
+let interviewList = [];
+let rejectedList = [];
+
+let total = document.getElementById('total');
+let interviewCount = document.getElementById('interviewCount');
+let rejectCount = document.getElementById('rejectCount');
+
+function calculateCount(){
+    const allCards = document.getElementById('allCards').children.length;
+    total.innerText = allCards;
+
+
+    interviewCount.innerText = interviewList.length;
+    rejectCount.innerText = rejectedList.length;
+}
+
+calculateCount();
+
+
+
+
